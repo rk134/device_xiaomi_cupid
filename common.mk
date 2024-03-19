55668c085498b9ce5d1aa7be8d4cba1e8c4926dc
Arian <arian.kulmer@web.de>
2024-03-19 22:46:42 +0100

    sm8450-common: Implement and build udfpshandler
    * De-commonize android.hardware.biometrics.fingerprint@2.3-service.xiaomi
      as only diting/cupid use it.
    
    sm8450-common: udfps: Keep fod_status enabled during enroll
    sm8450-common: udfps: Enable HBM via disp_feature
    sm8450-common: udfps: Use MI_DISP_PRIMARY as touch id
    sm8450-common: udfps: Improve fod press poll logging
    sm8450-common: udfps: Correct vendorCodes
    sm8450-common: udfps: Move HBM requests to fod press status listener
    sm8450-common: udfps: Defer extCmd until fod ui is ready
    
    This does not work with xiaomi's original display driver,
    since partial reads from it are broken and it rejects all
    reads whose size is insufficient to get the whole response.
    However, the size is not known in prior, so we actually
    want to read the header first to determine the actual size
    and continue reading then. This requires a kernel side fix
    like [1].
    
    To make this work with the stock kernel driver instead,
    adjust the parseDispEvent method to read with a larger
    buffer directly and decrease the size of the data
    according to the header later.
    
    [1]: https://github.com/cupid-development/android_kernel_xiaomi_sm8450-modules/commit/b80cb5a0289b3f37217939123087d7d45354bed4
    
    sm8450-common: udfps: Drop unnecessary setFingerDown on cancel
    
    HBM is usually not on when it gets cancelled
    
    sm8450-common: udfps: Add comments about setFingerDown usage
    sm8450-common: udfps: Use 110 nit fod when the brightness is low
    sm8450-common: udfps: Inform hwmodule about press coordinates
    
    These coordinates can be from the last press, ideally we would
    get the coordinates freshly when fod_press_status changes.
    
    sm8450-common: udfps: Change verbose logging to DEBUG
    sm8450-common: udfps: Disable HBM directly on successfull authentication
    sm8450-common: udfps: Enable fod_status on finger down events instead for fpc_fod
    
    Unfortunately, the fpc_fod hwmodule does not send vendor messages
    when it starts to wait for fingerprints reliably...
    
    sm8450-common: udfps: Don't try to read with a negative length
    sm8450-common: udfps: Enforce high brightness while enrolling
    sm8450-common: udfps: Always use max fod brightness
    
    using 110nit fod on low brightness significantly regresses fod unlock speeds
    
    This commit reverts 43650d15a4d90be5c2393621a164538df34471fd
    
    sm8450-common: udfps: Avoid partial reads on disp_feature
    
    As mentioned in 9043fcb8e4c6baf6a85af717a299369931f6e697, this is needed to make parseDispEvent work with prebuilt kernel modules.
    
    co-authored-by: kxxt <rsworktech@outlook.com>
    co-authored-by: bheatleyyy <sugakesshaa@pixelos.net>
    
    Change-Id: I4e90632dc2f756ac030cda8381c4e3f3c2f588a7

--- common.mk
+++ common.mk
@@ -181,6 +181,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    libudfpshandler
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
