a214349990c50e474d2609df9ad6f7685c774197
Arian <arian.kulmer@web.de>
2024-04-21 19:49:17 +0200

    sm8450-common: Implement sensors slpi notifying for light sensor
    Change-Id: I76c7479623433946bd0d1dfa372d06ee01fc080e

--- common.mk
+++ common.mk
@@ -367,7 +367,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    libsensorndkbridge
+    libsensorndkbridge \
+    disp2slpi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
